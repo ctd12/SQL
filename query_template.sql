13967b4278f0cd06e8ddd05ac794d7813bc79ea3
Casey Delaney <119380878+ctd12@users.noreply.github.com>
2024-02-13 11:58:15 -0500

    Add files via upload

---
+++ query_template.sql
@@ -0,0 +1,29 @@
+/*
+***********************************************************************
+File: name_here.sql
+
+Input Parameters:
+	Enter Here
+
+Tables/Views:
+	Start Here
+
+Creates Table/View:
+	Enter Here
+
+Functions:
+	Enter Here
+
+Author: Your Name Here
+
+Department: Patient Financial Services
+
+Purpose/Description
+	Entere Here
+
+Revision History:
+Date		Version		Description
+----		----		----
+2018-07-13	v1			Initial Creation
+***********************************************************************
+*/
